ae5a29843771dbb985ed28716ea8dc07972ff759
Evan <evandash6@gmail.com>
2026-08-04 01:43:16 -0600

    Ultimos cambios y responsive (falta guardar formulario)

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,10 +15,10 @@ $(function() {
         var scroll = $(window).scrollTop();
         if (scroll < 20) {
             $(".header_navbar").removeClass("sticky");
-            $(".header_navbar img").attr("src", "assets/images/logo.png");
+            $(".header_navbar img").attr("src", "assets/images/logos_b.png");
         } else {
             $(".header_navbar").addClass("sticky");
-            $(".header_navbar img").attr("src", "assets/images/logo-2.png");
+            $(".header_navbar img").attr("src", "assets/images/logos_b.png");
         }
     });
     
