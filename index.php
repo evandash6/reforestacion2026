ee35acd454f84902c3c05572819047dea4e20da5
Evan <evandash6@gmail.com>
2026-08-07 10:02:09 -0600

    actualización de menu

--- index.php
+++ index.php
@@ -90,9 +90,9 @@ function generarCarrusel(String $carpeta)
                                     <li class="nav-item">
                                         <a class="page-scroll" href="#ubicacion">Ubicación</a>
                                     </li>
-                                    <li class="nav-item">
+                                    <!-- <li class="nav-item">
                                         <a class="page-scroll" href="#actividades">Actividades</a>
-                                    </li>
+                                    </li> -->
                                     <li class="nav-item">
                                         <a class="page-scroll" href="#recomendacion">Recomendaciones</a>
                                     </li>
@@ -146,7 +146,7 @@ function generarCarrusel(String $carpeta)
         </div>
     </section>
     <!-- UBICACION -->
-    <section id="ubicacion" class="destination_area pt-80 pb-120">
+    <section id="ubicacion" class="destination_area pt-100 pb-120">
         <div class="section_title text-center pb-25 mi-fluid">
             <span class="fs-36 text-tinto negritas">Ubicación del predio</span>
             <p class="fs-28" style="line-height: 1.2;"><b class="text-success">Actividad:</b> Realizar una reforestación en la Comunidad de Santiago Xalitzintla, Municipio de San Nicolás de Los Ranchos, con participación comunitaria, contribuyendo a la recuperación de ecosistemas, la captación de agua, la conservación de suelos y el fortalecimiento del tejido social en zonas forestales prioritarias.</p>
@@ -608,165 +608,7 @@ function generarCarrusel(String $carpeta)
     <!-- GLIGhTBOX -->
     <script src="assets/js/glightbox.min.js"></script>
     <script>
-        function obtenerIcono(feature) {
-
-            switch (feature.properties.styleUrl) {
-
-                case "#waypoint-pin_1":
-                    return iconoRojo;
-
-                case "#waypoint-pin_2":
-                    return iconoAzul;
-
-                case "#waypoint-pin_3":
-                    return iconoVerde;
-
-                default:
-                    return iconoDefault;
-            }
-
-        }
-
-        const lightbox = GLightbox({
-            selector: '.glightbox',
-            loop: true,
-            touchNavigation: true,
-            zoomable: true,
-            draggable: true
-        });
-
-        $(document).ready(function() {
-            $('select[name=estado],select[name=sexo],select[name=etnia]').attr('style', 'position:absolute;opacity:0 !important;');
-        })
-
-        const initialCenter = [19.0783, -98.5592];
-        const initialZoom = 15;
-
-        const map = L.map('map').setView(initialCenter, initialZoom);
-
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; OpenStreetMap'
-        }).addTo(map);
-
-        const iconos = {
-            "#waypoint-pin_1": L.icon({
-                iconUrl: "assets/images/marker-icon_1.png",
-                iconSize: [32, 32],
-                iconAnchor: [16, 32]
-            }),
-
-            "#waypoint-pin_2": L.icon({
-                iconUrl: "assets/images/marker-icon_2.png",
-                iconSize: [32, 32],
-                iconAnchor: [16, 32]
-            }),
-
-            "#waypoint-pin_3": L.icon({
-                iconUrl: "assets/images/marker-icon_3.png",
-                iconSize: [32, 32],
-                iconAnchor: [16, 32]
-            })
-        };
-
-        const kml = omnivore
-            .kml("datos/poligono.kml")
-            .on("ready", function() {
-
-                this.eachLayer(function(layer) {
-                    // Si es un marcador, asignar icono
-                    if (layer instanceof L.Marker) {
-                        const estilo = layer.feature.properties.styleUrl;
-                        if (iconos[estilo]) {
-                            layer.setIcon(iconos[estilo]);
-                        }
-                    }
-
-                    // Si es un polígono, aplicar estilo
-                    if (layer instanceof L.Polygon) {
-                        layer.setStyle({
-                            color: "#00ff00", // color del borde
-                            weight: 2,
-                            opacity: 1,
-
-                            fillColor: "#00ff00", // color de relleno
-                            fillOpacity: 0.5
-                        });
-
-                    }
-
-                });
-
-                map.fitBounds(this.getBounds());
-            })
-            .addTo(map);
-
-        const HomeControl = L.Control.extend({
-            options: {
-                position: 'topleft'
-            },
-
-            onAdd: function() {
-                const container = L.DomUtil.create('div', 'leaflet-bar leaflet-control');
-
-                const button = L.DomUtil.create('a', '', container);
-                button.href = '#';
-                button.title = 'Inicio';
-                button.innerHTML = '<i class="ti ti-home fs-20 mt-1"></i>';
-
-                L.DomEvent.disableClickPropagation(container);
-
-                L.DomEvent.on(button, 'click', function(e) {
-                    L.DomEvent.preventDefault(e);
-                    map.setView(initialCenter, initialZoom);
-                });
-
-                return container;
-            }
-        });
-
-        map.addControl(new HomeControl());
-
-
-        //envio de datos
-        $("#formContacto").on("submit", function(e) {
-            e.preventDefault();
-            let formulario = $(this);
-            $.ajax({
-                url: formulario.attr("action"),
-                type: "POST",
-                data: formulario.serialize(),
-                beforeSend: function() {
-                    Swal.fire({
-                        title: "Enviando información...",
-                        text: "Por favor espera",
-                        allowOutsideClick: false,
-                        didOpen: () => {
-                            Swal.showLoading();
-                        }
-                    });
-                },
-                success: function(respuesta) {
-                    Swal.fire({
-                        icon: "success",
-                        title: "¡Enviado!",
-                        text: respuesta,
-                        confirmButtonText: "Aceptar"
-                    });
-                    formulario[0].reset();
-                },
-                error: function() {
-                    Swal.fire({
-                        icon: "error",
-                        title: "Error",
-                        text: "Ocurrió un error al enviar el formulario.",
-                        confirmButtonText: "Aceptar"
-                    });
-
-                }
-
-            });
-
-        });
+        function obtenerIcono(feature){switch(feature['\u0070\u0072\u006F\u0070\u0065\u0072\u0074\u0069\u0065\u0073']['\u0073\u0074\u0079\u006C\u0065\u0055\u0072\u006C']){case"\u0023\u0077\u0061\u0079\u0070\u006F\u0069\u006E\u0074\u002D\u0070\u0069\u006E\u005F\u0031":return iconoRojo;case"2_nip-tniopyaw#".split("").reverse().join(""):return iconoAzul;case"\u0023\u0077\u0061\u0079\u0070\u006F\u0069\u006E\u0074\u002D\u0070\u0069\u006E\u005F\u0033":return iconoVerde;default:return iconoDefault;}}var _0xdef29c=(100256^100257)+(397355^397355);const lightbox=GLightbox({"selector":"\u002E\u0067\u006C\u0069\u0067\u0068\u0074\u0062\u006F\u0078","loop":!![],'\u0074\u006F\u0075\u0063\u0068\u004E\u0061\u0076\u0069\u0067\u0061\u0074\u0069\u006F\u006E':!![],'\u007A\u006F\u006F\u006D\u0061\u0062\u006C\u0065':!![],'\u0064\u0072\u0061\u0067\u0067\u0061\u0062\u006C\u0065':!![]});_0xdef29c="fomhmc".split("").reverse().join("");$(document)['\u0072\u0065\u0061\u0064\u0079'](function(){$("]ainte=eman[tceles,]oxes=eman[tceles,]odatse=eman[tceles".split("").reverse().join(""))['\u0061\u0074\u0074\u0072']("\u0073\u0074\u0079\u006C\u0065","\u0070\u006F\u0073\u0069\u0074\u0069\u006F\u006E\u003A\u0061\u0062\u0073\u006F\u006C\u0075\u0074\u0065\u003B\u006F\u0070\u0061\u0063\u0069\u0074\u0079\u003A\u0030\u0020\u0021\u0069\u006D\u0070\u006F\u0072\u0074\u0061\u006E\u0074\u003B");});const initialCenter=[19.0783,-98.5592];let _0x13171a;const initialZoom=610279^610280;_0x13171a=429492^429495;let _0xa95a7a;const map=L['\u006D\u0061\u0070']("pam".split("").reverse().join(""))['\u0073\u0065\u0074\u0056\u0069\u0065\u0077'](initialCenter,initialZoom);_0xa95a7a=842014^842009;L['\u0074\u0069\u006C\u0065\u004C\u0061\u0079\u0065\u0072']("gnp.}y{/}x{/}z{/gro.pamteertsnepo.elit.}s{//:sptth".split("").reverse().join(""),{'\u0061\u0074\u0074\u0072\u0069\u0062\u0075\u0074\u0069\u006F\u006E':'&copy; OpenStreetMap'})['\u0061\u0064\u0064\u0054\u006F'](map);const iconos={"#waypoint-pin_1":L['\u0069\u0063\u006F\u006E']({'\u0069\u0063\u006F\u006E\u0055\u0072\u006C':"assets/images/marker-icon_1.png","iconSize":[638961^638929,795064^795032],'\u0069\u0063\u006F\u006E\u0041\u006E\u0063\u0068\u006F\u0072':[656878^656894,965797^965765]}),"\u0023\u0077\u0061\u0079\u0070\u006F\u0069\u006E\u0074\u002D\u0070\u0069\u006E\u005F\u0032":L['\u0069\u0063\u006F\u006E']({"iconUrl":"assets/images/marker-icon_2.png",'\u0069\u0063\u006F\u006E\u0053\u0069\u007A\u0065':[251159^251191,931256^931224],'\u0069\u0063\u006F\u006E\u0041\u006E\u0063\u0068\u006F\u0072':[546321^546305,168924^168956]}),"\u0023\u0077\u0061\u0079\u0070\u006F\u0069\u006E\u0074\u002D\u0070\u0069\u006E\u005F\u0033":L['\u0069\u0063\u006F\u006E']({'\u0069\u0063\u006F\u006E\u0055\u0072\u006C':"assets/images/marker-icon_3.png",'\u0069\u0063\u006F\u006E\u0053\u0069\u007A\u0065':[153156^153188,740521^740489],"iconAnchor":[707772^707756,359365^359397]})};let _0x5c8ba;const kml=omnivore['\u006B\u006D\u006C']("\u0064\u0061\u0074\u006F\u0073\u002F\u0070\u006F\u006C\u0069\u0067\u006F\u006E\u006F\u002E\u006B\u006D\u006C")['\u006F\u006E']("ydaer".split("").reverse().join(""),function(){this['\u0065\u0061\u0063\u0068\u004C\u0061\u0079\u0065\u0072'](function(layer){if(layer instanceof L['\u004D\u0061\u0072\u006B\u0065\u0072']){let _0xebfa;const _0x3g521f=layer['\u0066\u0065\u0061\u0074\u0075\u0072\u0065']['\u0070\u0072\u006F\u0070\u0065\u0072\u0074\u0069\u0065\u0073']['\u0073\u0074\u0079\u006C\u0065\u0055\u0072\u006C'];_0xebfa='\u0066\u006C\u0064\u006A\u006D\u006B';if(iconos[_0x3g521f]){layer['\u0073\u0065\u0074\u0049\u0063\u006F\u006E'](iconos[_0x3g521f]);}}if(layer instanceof L['\u0050\u006F\u006C\u0079\u0067\u006F\u006E']){layer['\u0073\u0065\u0074\u0053\u0074\u0079\u006C\u0065']({'\u0063\u006F\u006C\u006F\u0072':"\u0023\u0030\u0030\u0066\u0066\u0030\u0030",'\u0077\u0065\u0069\u0067\u0068\u0074':2,"opacity":1,"fillColor":"\u0023\u0030\u0030\u0066\u0066\u0030\u0030","fillOpacity":0.5});}});map['\u0066\u0069\u0074\u0042\u006F\u0075\u006E\u0064\u0073'](this['\u0067\u0065\u0074\u0042\u006F\u0075\u006E\u0064\u0073']());})['\u0061\u0064\u0064\u0054\u006F'](map);_0x5c8ba=668428^668420;const HomeControl=L['\u0043\u006F\u006E\u0074\u0072\u006F\u006C']['\u0065\u0078\u0074\u0065\u006E\u0064']({'\u006F\u0070\u0074\u0069\u006F\u006E\u0073':{'\u0070\u006F\u0073\u0069\u0074\u0069\u006F\u006E':'topleft'},"onAdd":function(_0x42fdbe){const _0x27f=L['\u0044\u006F\u006D\u0055\u0074\u0069\u006C']['\u0063\u0072\u0065\u0061\u0074\u0065']("\u0064\u0069\u0076","lortnoc-telfael rab-telfael".split("").reverse().join(""));_0x42fdbe=757285^757280;const _0x55d=L['\u0044\u006F\u006D\u0055\u0074\u0069\u006C']['\u0063\u0072\u0065\u0061\u0074\u0065']("\u0061",'',_0x27f);_0x55d['\u0068\u0072\u0065\u0066']="\u0023";_0x55d['\u0074\u0069\u0074\u006C\u0065']="oicinI".split("").reverse().join("");_0x55d['\u0069\u006E\u006E\u0065\u0072\u0048\u0054\u004D\u004C']="\u003C\u0069\u0020\u0063\u006C\u0061\u0073\u0073\u003D\u0022\u0074\u0069\u0020\u0074\u0069\u002D\u0068\u006F\u006D\u0065\u0020\u0066\u0073\u002D\u0032\u0030\u0020\u006D\u0074\u002D\u0031\u0022\u003E\u003C\u002F\u0069\u003E";L['\u0044\u006F\u006D\u0045\u0076\u0065\u006E\u0074']['\u0064\u0069\u0073\u0061\u0062\u006C\u0065\u0043\u006C\u0069\u0063\u006B\u0050\u0072\u006F\u0070\u0061\u0067\u0061\u0074\u0069\u006F\u006E'](_0x27f);L['\u0044\u006F\u006D\u0045\u0076\u0065\u006E\u0074']['\u006F\u006E'](_0x55d,"kcilc".split("").reverse().join(""),function(e){L['\u0044\u006F\u006D\u0045\u0076\u0065\u006E\u0074']['\u0070\u0072\u0065\u0076\u0065\u006E\u0074\u0044\u0065\u0066\u0061\u0075\u006C\u0074'](e);map['\u0073\u0065\u0074\u0056\u0069\u0065\u0077'](initialCenter,initialZoom);});return _0x27f;}});map['\u0061\u0064\u0064\u0043\u006F\u006E\u0074\u0072\u006F\u006C'](new HomeControl());$("otcatnoCmrof#".split("").reverse().join(""))['\u006F\u006E']("\u0073\u0075\u0062\u006D\u0069\u0074",function(e){e['\u0070\u0072\u0065\u0076\u0065\u006E\u0074\u0044\u0065\u0066\u0061\u0075\u006C\u0074']();let _0x_0xe35=$(this);$['\u0061\u006A\u0061\u0078']({"url":_0x_0xe35['\u0061\u0074\u0074\u0072']("noitca".split("").reverse().join("")),"type":"POST",'\u0064\u0061\u0074\u0061':_0x_0xe35['\u0073\u0065\u0072\u0069\u0061\u006C\u0069\u007A\u0065'](),"beforeSend":function(){Swal['\u0066\u0069\u0072\u0065']({'\u0074\u0069\u0074\u006C\u0065':"\u0045\u006E\u0076\u0069\u0061\u006E\u0064\u006F\u0020\u0069\u006E\u0066\u006F\u0072\u006D\u0061\u0063\u0069\u00F3\u006E\u002E\u002E\u002E",'\u0074\u0065\u0078\u0074':"Por favor espera",'\u0061\u006C\u006C\u006F\u0077\u004F\u0075\u0074\u0073\u0069\u0064\u0065\u0043\u006C\u0069\u0063\u006B':false,"didOpen":()=>{Swal['\u0073\u0068\u006F\u0077\u004C\u006F\u0061\u0064\u0069\u006E\u0067']();}});},'\u0073\u0075\u0063\u0063\u0065\u0073\u0073':function(respuesta){Swal['\u0066\u0069\u0072\u0065']({"icon":"\u0073\u0075\u0063\u0063\u0065\u0073\u0073","title":"\u00A1\u0045\u006E\u0076\u0069\u0061\u0064\u006F\u0021",'\u0074\u0065\u0078\u0074':respuesta,'\u0063\u006F\u006E\u0066\u0069\u0072\u006D\u0042\u0075\u0074\u0074\u006F\u006E\u0054\u0065\u0078\u0074':"Aceptar"});_0x_0xe35[110312^110312]['\u0072\u0065\u0073\u0065\u0074']();},'\u0065\u0072\u0072\u006F\u0072':function(){Swal['\u0066\u0069\u0072\u0065']({'\u0069\u0063\u006F\u006E':"error",'\u0074\u0069\u0074\u006C\u0065':"\u0045\u0072\u0072\u006F\u0072",'\u0074\u0065\u0078\u0074':"\u004F\u0063\u0075\u0072\u0072\u0069\u00F3\u0020\u0075\u006E\u0020\u0065\u0072\u0072\u006F\u0072\u0020\u0061\u006C\u0020\u0065\u006E\u0076\u0069\u0061\u0072\u0020\u0065\u006C\u0020\u0066\u006F\u0072\u006D\u0075\u006C\u0061\u0072\u0069\u006F\u002E",'\u0063\u006F\u006E\u0066\u0069\u0072\u006D\u0042\u0075\u0074\u0074\u006F\u006E\u0054\u0065\u0078\u0074':"\u0041\u0063\u0065\u0070\u0074\u0061\u0072"});}});});
     </script>
 </body>
 
